c89694c76cdffb953bfc046ada314acd0bfc298e
LUCAS ARAGÃO <lucas.aragao2@etec.sp.gov.br>
2025-08-20 17:05:34 -0300

    Remove Login and Register components, and set Home as initial route

--- App.js
+++ App.js
@@ -3,18 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Importando as telas
-import Login from './src/components/login';
-import Register from './src/components/register';
+
+import Home from './src/components/Home';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
